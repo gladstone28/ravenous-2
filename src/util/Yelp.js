5c8af5d9bf830fecca32ec690217179ac5a2313f
Gladstone Rose <gladstonerose@hotmail.com>
2024-11-07 23:24:34 -0500

    modify Yelp.js

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,4 +1,4 @@
-const apiKey = 'YOUR_YELP_API_KEY';
+const apiKey = 'v0ZG6vWt943ZgreGwvkesMa2TebNl6vGf3HjS8EN0T5-tC4oUY37bySKvrdCMPsytfcU0n5F9mFSuPdvMOg7Bn6n_N2dDtaZCBj1okP-kPsUh89V5A7iDlalL48tZ3Yx';
 
 const Yelp = {
   search(term, location, sortBy) {
